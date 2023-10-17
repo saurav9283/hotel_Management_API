34acc28b5e96f4d81859b36c325488bcb82c1e6a
Saurav <sauravkumar00108@gmail.com>
2023-10-17 23:48:39 +0530

    api changes

--- routes/hotels.js
+++ routes/hotels.js
@@ -14,27 +14,19 @@ import { verifyAdmin } from "../utils/VeriftToken.js";
 
 const router = express.Router();
 
-//create
-// router.post("/",verifyAdmin, createHotel)
-// //update
-// router.put("/:id",verifyAdmin, updatedHotel)
-// //delete
-// router.delete("/find/:id",verifyAdmin,deleteHotel)
-// //get specific hotel
-// //get all hotels
-// router.get("/", getHotels)
-// router.get("/countByCity", countByCity)
-// router.get("/countByType", countByType)
-// router.get("/room/:id",gethotelRooms)
-// router.get("/:id",getHotel)
-router.get("/", async (req, res) => {
-  try {
-    const hotels = await Hotel.find({});
-    res.status(200).json(hotels);
-  } catch (err) {
-    res.send(err)
-    // console.log("error");
-  }
-});
+// create
+router.post("/",verifyAdmin, createHotel)
+//update
+router.put("/:id",verifyAdmin, updatedHotel)
+//delete
+router.delete("/find/:id",verifyAdmin,deleteHotel)
+//get specific hotel
+//get all hotels
+router.get("/", getHotels)
+router.get("/countByCity", countByCity)
+router.get("/countByType", countByType)
+router.get("/room/:id",gethotelRooms)
+router.get("/:id",getHotel)
+
 
 export default router;
