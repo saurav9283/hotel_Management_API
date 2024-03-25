01ad2d42c27426cda930252cad397ba83ed662f2
Saurav <sauravkumar00108@gmail.com>
2024-03-25 21:05:50 +0530

    Fix email formatting and add password expiration email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,60 +1,80 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import nodemailer from "nodemailer"
-import dotenv from "dotenv"
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  }
+  },
 });
 
 // this  function is used to send mail
-async function sendEmail(to,subject,text){
+async function sendEmail(to, subject, htmlContent) {
   try {
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
-      to,subject,text
+      to,
+      subject,
+      html:htmlContent,      
     });
     console.log("Email send suceddful");
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error("Error sending email:", error);
   }
 }
 
 export const register = async (req, res, next) => {
   try {
-    const {username,email,password} = req.body;
-    const user = await User.findOne({username})
-    if(user)
-    {
-      return res.send({msg:"Username already Exist!"});
+    const { username, email, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.send({ msg: "Username already Exist!" });
     }
-    const emailExist = await User.findOne({email})
-    if(emailExist)
-    {
-      return res.send({msg:"Try to other email"})
+    const emailExist = await User.findOne({ email });
+    if (emailExist) {
+      return res.send({ msg: "Try to other email" });
     }
-    const hashPassword = await bcrypt.hash(password,10)
-    const newUser = new User({username,email,password:hashPassword,});
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashPassword });
     await newUser.save();
-    await sendEmail(email, 'Registration Successful', 'Welcome to our platform! You have successfully registered.');
+    const htmlContent = `
+      <html>
+        <head>
+          <style>
+            .heading{
+              color: blue;
+              text-align: center;
+            }
+          </style>
+        </head>
+        <body>
+          <h1 className="heading">Welcome to our Staying!</h1>
+          <p>You have successfully registered and your time to search Hotel is our first priority.</p>
+        </body>
+      </html>
+    `;
 
-    res.status(201).send({mag:"User has been created!"});
+    await sendEmail(
+      email,
+      "Registration Successful",
+      htmlContent
+    );
 
+    res.status(201).send({ mag: "User has been created!" });
   } catch (error) {
-    console.log("ERROR",error)
-    res.status(500).send({msg:"Registration failed!"})
+    console.log("ERROR", error);
+    res.status(500).send({ msg: "Registration failed!" });
   }
-
 };
 
 export const login = async (req, res, next) => {
   try {
+    const currentDate = new Date();
     const user = await User.findOne({ username: req.body.username });
     if (!user) {
       return res.send({ message: "User not found!" });
@@ -67,6 +87,19 @@ export const login = async (req, res, next) => {
     if (!ispasswordCorrect) {
       return res.send({ message: "Wrong Password or Username!" });
     }
+    const diffInMilliseconds = Math.abs(currentDate - user.createdAt);
+    const diffInDays = Math.ceil(diffInMilliseconds / (1000 * 60 * 60 * 24));
+    const passwordExpIn = 28 - diffInDays;
+
+    // console.log(`Password has been created ${diffInDays} ,${passwordExpIn} days ago`);
+
+    if (passwordExpIn <= 1) {
+      await sendEmail(
+        user.email,
+        "Password Reset",
+        "Please reset your password"
+      );
+    }
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
@@ -74,11 +107,11 @@ export const login = async (req, res, next) => {
     );
     const { password, isAdmin, ...otherDetails } = user._doc;
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie("access_token", token,passwordExpIn, { httpOnly: true })
       .status(200)
       .send({ ...otherDetails });
   } catch (err) {
-    console.error(err); // Log the error for debugging
+    console.error(err);
     res.status(500).send("Error creating the user.");
   }
 };
